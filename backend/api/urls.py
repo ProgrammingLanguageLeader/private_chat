c55aedeba3e44e9d8b61cf4049e0b57be9aebec6
Rudakov Vladimir <vladimirrudd@gmail.com>
2018-07-03 15:15:49 +0300

    Created SignIn Component

--- backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-import api.views as chat_views
-
-
-urlpatterns = [
-    path('get_messages/', chat_views.MessagesListView.as_view()),
-]
---
+++ front/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css';
+import Logo from './components/Logo/Logo';
+import 'tachyons';
+import Signin from './components/Signin/Signin';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      route: ''
+    }
+  }
+
+  onRouteChange = (route) => {
+    this.setState({route: route});
+  }
+
+  render() {
+    return (
+      <div>
+        { this.state.route === 'home'
+          ? <div><p className='f1 pa2 white' onClick={() => this.onRouteChange('')}>You are logged in! (click to Sign Out)</p></div>
+          : <Logo onRouteChange={this.onRouteChange} />
+        }
+        { this.state.route === 'signin'
+          ? <Signin onRouteChange={this.onRouteChange} />
+          : <div></div>
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
