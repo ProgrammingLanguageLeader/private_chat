a2b676267d38d781b5bdff48cf48899b1cbd7c1e
ProgrammingLanguageLeader <vip.shoroch@gmail.com>
2018-07-11 19:32:31 +0300

    user model updated

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,12 +1,32 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    UserManager, AbstractBaseUser, PermissionsMixin
+)
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(
+        unique=True,
+        max_length=100
+    )
+    email = models.EmailField(
+        unique=True,
+        max_length=254,
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
     public_email = models.BooleanField(default=False)
     public_first_name = models.BooleanField(default=True)
     public_last_name = models.BooleanField(default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
 
 
 class Message(models.Model):
